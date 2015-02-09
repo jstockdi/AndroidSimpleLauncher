3863f9a1b52be184c1c3ef10d1daf755a8f12399
Jonathan Stockdill <jons@kenzanmedia.com>
2015-02-08 20:32:56 -0500

    Adding bluetooth functionality as well

--- app/src/main/java/com/kenzan/simplelauncher/HomeActivity.java
+++ app/src/main/java/com/kenzan/simplelauncher/HomeActivity.java
@@ -1,25 +1,194 @@
 package com.kenzan.simplelauncher;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.content.IntentFilter;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
 
 
 public class HomeActivity extends Activity {
+
+
+    private static final int REQUEST_ENABLE_BT = 1;
+
+    private Button onBtn;
+    private Button offBtn;
+    private Button listBtn;
+    private Button findBtn;
+
+    private TextView text;
+
+    private BluetoothAdapter myBluetoothAdapter;
+    private Set<BluetoothDevice> pairedDevices;
+
+    private ListView myListView;
+
+    private ArrayAdapter<String> btarrayadapter;
+
+
+    final BroadcastReceiver bReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // add the name and the MAC address of the object to the arrayAdapter
+                btarrayadapter.add(device.getName() + "\n" + device.getAddress());
+                btarrayadapter.notifyDataSetChanged();
+            }
+        }
+    };
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+        // take an instance of BluetoothAdapter - Bluetooth radio
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        text = (TextView) findViewById(R.id.text);
+        onBtn = (Button)findViewById(R.id.turnOn);
+        offBtn = (Button)findViewById(R.id.turnOff);
+        listBtn = (Button)findViewById(R.id.paired);
+        findBtn = (Button)findViewById(R.id.search);
+
+        if(myBluetoothAdapter == null) {
+            onBtn.setEnabled(false);
+            offBtn.setEnabled(false);
+            listBtn.setEnabled(false);
+            findBtn.setEnabled(false);
+            text.setText("Status: not supported");
+
+            Toast.makeText(getApplicationContext(), "Your device does not support Bluetooth",
+                    Toast.LENGTH_LONG).show();
+        } else {
+
+
+            onBtn.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    on(v);
+                }
+            });
+
+
+            offBtn.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    off(v);
+                }
+            });
+
+
+            listBtn.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    list(v);
+                }
+            });
+
+
+            findBtn.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    find(v);
+                }
+            });
+
+            myListView = (ListView)findViewById(R.id.listView1);
+
+            // create the arrayAdapter that contains the BTDevices, and set it to the ListView
+            btarrayadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+            myListView.setAdapter(btarrayadapter);
+        }
+    }
+
+    public void on(View view){
+        if (!myBluetoothAdapter.isEnabled()) {
+            Intent turnOnIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOnIntent, REQUEST_ENABLE_BT);
+
+            Toast.makeText(getApplicationContext(),"Bluetooth turned on" ,
+                    Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Bluetooth is already on",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 
     public void showApps(View v){
         Intent i = new Intent(this, AppsListActivity.class);
         startActivity(i);
     }
+
+    public void off(View view){
+        myBluetoothAdapter.disable();
+        text.setText("Status: Disconnected");
+
+        Toast.makeText(getApplicationContext(),"Bluetooth turned off",
+                Toast.LENGTH_LONG).show();
+    }
+
+
+    public void list(View view){
+        // get paired devices
+        pairedDevices = myBluetoothAdapter.getBondedDevices();
+
+        // put it's one to the adapter
+        for(BluetoothDevice device : pairedDevices)
+            btarrayadapter.add(device.getName()+ "\n" + device.getAddress());
+
+        Toast.makeText(getApplicationContext(),"Show Paired Devices",
+                Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void find(View view) {
+        if (myBluetoothAdapter.isDiscovering()) {
+            // the button is pressed when it discovers, so cancel the discovery
+            myBluetoothAdapter.cancelDiscovery();
+        }
+        else {
+            btarrayadapter.clear();
+            myBluetoothAdapter.startDiscovery();
+
+            registerReceiver(bReceiver, new IntentFilter(BluetoothDevice.ACTION_FOUND));
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_ENABLE_BT){
+            if(myBluetoothAdapter.isEnabled()) {
+                text.setText("Status: Enabled");
+            } else {
+                text.setText("Status: Disabled");
+            }
+        }
+    }
+
+
 }
