a1526ed17ca573e9f5f4d8fe5e0c40c579912a86
Jonathan Stockdill <jons@kenzanmedia.com>
2015-02-08 19:20:19 -0500

    Initial Commit

---
+++ app/src/main/java/com/kenzan/simplelauncher/HomeActivity.java
@@ -0,0 +1,25 @@
+package com.kenzan.simplelauncher;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class HomeActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void showApps(View v){
+        Intent i = new Intent(this, AppsListActivity.class);
+        startActivity(i);
+    }
+}
